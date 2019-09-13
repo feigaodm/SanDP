27e63ca733529e54cdb3c06a46727f0d6bb16f11
Fei Gao <feigao.ge@gmail.com>
2019-09-13 18:40:19 +0200

    0.06

--- sandp/calvariables.py
+++ sandp/calvariables.py
@@ -386,7 +386,7 @@ def processSPE(filename, outpath):
             print('SPE TEST: '+str(spe_potential))
             spe_boundary += spe_potential
 
-            SPEs = integral(spe_potential, channel[i], BaseLineChannel[i], PMTgain[i])
+            SPEs = integral(spe_potential, channel[ich], BaseLineChannel[ich], PMTgain[ich])
             print(SPEs)
             # Number of SPE:
             # NbS1Peaks[0] = len(spe_potential)
