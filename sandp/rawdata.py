917def9c0c8f8bfc5fc99d546c52af1bb582e0b3
Fei Gao <feigao.ge@gmail.com>
2019-09-13 16:30:15 +0200

    0.06

--- sandp/rawdata.py
+++ sandp/rawdata.py
@@ -36,7 +36,7 @@ def get_raw(event_number, filename):
         # if event_number == 0:
         #    return length_unit ## This offset is for the Unix before the header of the first event.
         # return event_number*length_unit*6 + event_number*nsamps*nchs*length_unit/2
-        return (event_number)*(nsamps*nchs*4/2+28)
+        return event_number*(nsamps*nchs*4/2+28)
         ##        UnixTime + event_number*Header        + event_number*samples
         ##        (note: samples need to be /2 according to the CAEN manual)
 
@@ -46,12 +46,12 @@ def get_raw(event_number, filename):
     ## Last event:
     seek_number = calculate_seek_number(event_number - 1) ## -1 means last event
     print('seek_number: '+str(seek_number))
-    ss.seek(seek_number + length_unit*7) ## 5*length_unit offset to TRIGGER TIME TAG
+    ss.seek(seek_number + length_unit*7) ## 7*length_unit offset to TRIGGER TIME TAG
     pre_counter=struct.unpack('i',ss.read(4))[0] & 0xffffffff
     
     ## This event:
     seek_number = calculate_seek_number(event_number)
-    ss.seek(seek_number + length_unit*7) ## 5*length_unit offset to TRIGGER TIME TAG
+    ss.seek(seek_number + length_unit*7) ## 7*length_unit offset to TRIGGER TIME TAG
     counter=struct.unpack('i',ss.read(4))[0] & 0xffffffff
     
     ## Calculating the Trigger Time difference between LAST and THIS event:
@@ -70,8 +70,8 @@ def get_raw(event_number, filename):
         data_tmp=[]
         for i in range(nsamps/2):
             tmp=struct.unpack("i",ss.read(4))[0]
-            data_tmp.append((tmp >> 16)*2.0/4096)
-            data_tmp.append((tmp & 0x0000ffff)*2.0/4096)
+            data_tmp.append((tmp >> 16)*0.5/16384)
+            data_tmp.append((tmp & 0x0000ffff)*0.5/16384)
             
         channel.append([i for i in data_tmp])
         for i in range(nsamps):
